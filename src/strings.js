2bd3f49ff274e1542491ea4a3801a41a4f19cca1
nathanscott85 <nathansfeely@gmail.com>
2018-11-06 13:39:32 +0000

    added strings tests and functions

---
+++ src/strings.js
@@ -0,0 +1,11 @@
+function emptyString() {
+  // returns an empty string
+}
+
+function stringConcat() {
+  // returns a string concatinated together with no spaces
+}
+function stringConcatSpaced() {
+  // returns a longer concatinated string with spaces between each word
+}
+module.exports = { emptyString, stringConcat, stringConcatSpaced };
---
+++ test/strings.spec.js
@@ -0,0 +1,29 @@
+const { expect } = require("chai");
+
+const {
+  emptyString,
+  stringConcat,
+  stringConcatSpaced
+} = require("../src/strings");
+
+describe("empty array", () => {
+  it("returns an empty string", () => {
+    expect(emptyString("")).to.be.a("string").that.is.empty;
+  });
+});
+
+describe("string concat", () => {
+  it("returns an empty string", () => {
+    expect(stringConcat("string", "concatination")).to.eql(
+      "string concatination"
+    );
+  });
+});
+
+describe.only("string concat with spaces", () => {
+  it("returns an empty string", () => {
+    expect(
+      stringConcatSpaced("string", "concatination", "with", "spaces")
+    ).to.eql("string concatination with spaces");
+  });
+});
