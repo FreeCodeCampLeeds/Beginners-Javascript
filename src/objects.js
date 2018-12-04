8bd9bd93e4137b82f13c9819407d445a1b141485
nathanscott85 <nathansfeely@gmail.com>
2018-12-04 09:39:42 +0000

    changed object.js

--- src/objects.js
+++ src/objects.js
@@ -1,8 +1,7 @@
 function empty() {
   // returns an empty object
 }
-function objectKeys(obj) {
+function objectKeys() {
   //returns an array of keys from an object
-  return Object.keys(obj);
 }
 module.exports = { empty, objectKeys };
